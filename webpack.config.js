d1a0cc2328636ae9168e6b77cb0137ecc8fc0d9f
Frank Purcell <fxpurcell@gmail.com>
2017-11-28 11:08:38 -0800

    webpack crank out .html file

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@
 
 var path = require('path')
 var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './lib',
@@ -12,6 +13,23 @@ module.exports = {
         filename: '[name].js',
         publicPath: '/'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: 'lib/index.tpl.html',
+          inject: 'body',
+          filename: 'index.html'
+        }),
+
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+
+        // enable HMR globally
+        new webpack.HotModuleReplacementPlugin(),
+
+        // prints more readable module names in the browser console on HMR updates
+        new webpack.NamedModulesPlugin()
+    ],
     module: {
         loaders: [
             {
