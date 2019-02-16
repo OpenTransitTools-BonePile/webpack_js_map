6d9ba25173637a8e1095526f040fe9bde2cd4105
Frank Purcell <fxpurcell@gmail.com>
2019-02-16 03:51:31 -0800

    icon hover

--- apps/busses/index.js
+++ apps/busses/index.js
@@ -7,24 +7,40 @@ import config from "json-loader!yaml-loader!../config.yml";
 
 var mc = new MapController(config);
 
+const blackIcon = L.icon({
+    iconUrl: 'https://maps.trimet.org/images/map/misc/vehicle-arrow.png',
+    iconSize: [15, 15],
+    iconAnchor: [7, 7],
+    popupAnchor: [0, -6]
+});
+
+const orangeIcon = L.icon({
+    iconUrl: 'https://maps.trimet.org/images/map/misc/vehicle-arrow-on.png',
+    iconSize: [15, 15],
+    iconAnchor: [7, 7],
+    popupAnchor: [0, -6]
+});
+
 
 var geojsonLayer = new L.GeoJSON.AJAX("http://localhost:54145/vehicles_via_route?id=12", {
     pointToLayer: function(pt, latlng) {
-        console.log("marker at " + latlng);
         var lat = pt.properties.lat;
         var lon = pt.properties.lon;
         var ll = L.latLng(lat, lon);
-        var ic = L.icon({
-            iconUrl: 'https://maps.trimet.org/images/map/misc/vehicle-arrow.png',
-            iconSize: [15, 15],
-            iconAnchor: [7, 7],
-            popupAnchor: [0, -6]
-        });
+        console.log("marker at " + ll);
         return L.marker(ll, {
-                icon: ic,
+                icon: blackIcon,
                 rotationAngle: pt.properties.heading
             }
         );
+    },
+    onEachFeature: function(feature, layer) {
+        layer.on("mouseover",function(e) {
+            layer.setIcon(orangeIcon)
+        });
+        layer.on("mouseout",function(e) {
+            layer.setIcon(blackIcon)
+        });
     }
     }).bindPopup(function (layer) {
         console.log("popup");
